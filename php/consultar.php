3982158ab2b66e91658606ba2b40c5b148ef1b46
dwayne9078 <125242085+dwayne9078@users.noreply.github.com>
2023-07-18 01:34:15 -0600

    Add files via upload

---
+++ php/consultar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consultas</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+    <h1>Consultar Cliente y Pedido</h1>
+    <a href="menu.php">Regresar</a>
+
+    <?php
+    
+
+    
+    ?>
+    
+</body>
+</html>
---
+++ php/menu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Principal</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+    <h1>Menú Principal</h1>
+    <a href="formulario_cliente.php">Añadir Cliente</a>
+    <a href="formulario_pedido.php">Añadir Pedido</a>
+    <a href="consultar.php">Consultar Clientes y Pedidos</a>
+
+</body>
+</html>
